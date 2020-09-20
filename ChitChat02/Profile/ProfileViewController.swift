61ffc9f38808e3389d0d1f00429e2a9d41d2a695
Tim <the.tinted.knight@gmail.com>
2020-09-20 21:08:34 +0300

    - Choose from gallery - UIImageView: center image

--- ChitChat02/Profile/ProfileViewController.swift
+++ ChitChat02/Profile/ProfileViewController.swift
@@ -20,6 +20,8 @@ class ProfileViewController : ViewController {
     private let fakeUserName = "Timur Tharkahov"
     private let fakeUserDescription = "UX/UI designer, web-designer, Moscow, Russia"
     
+    private let picker = UIImagePickerController()
+
     override func viewDidLoad() {
         applog("ProfileViewController::\(#function)")
 
@@ -29,6 +31,10 @@ class ProfileViewController : ViewController {
     
     private func prepareUi() {
         buttonSave.layer.cornerRadius = 14.0
+        
+        profilePicture.layer.cornerRadius = profilePicture.bounds.height / 2
+        profilePicture.layer.masksToBounds = true
+        
         profilePicture.isUserInteractionEnabled = true
         profilePicture.addGestureRecognizer(
             UITapGestureRecognizer(target: self, action: #selector(onProfilePictureTap)))
@@ -45,20 +51,45 @@ class ProfileViewController : ViewController {
     
     private func showChooseDialog() {
         let alertController = UIAlertController(title: nil, message: "Think twice. Everyone all over the Internet will see your face.", preferredStyle: .actionSheet)
-        let cameraAction = UIAlertAction(title: "Make a foto", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+        let cameraAction = UIAlertAction(title: "Make a foto", style: .default) { UIAlertAction in
             applog("camera")
-        })
-        let galleryAction = UIAlertAction(title: "From galley", style: .default, handler: { (alert: UIAlertAction!) -> Void in
-            applog("galley")
-        })
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (alert: UIAlertAction!) -> Void in
+        }
+        let galleryAction = UIAlertAction(title: "From galley", style: .default) { UIAlertAction in
+            self.chooseFromGallery()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { UIAlertAction in
             applog("cancel")
-        })
+        }
         
         alertController.addAction(cameraAction)
         alertController.addAction(galleryAction)
         alertController.addAction(cancelAction)
         
         present(alertController, animated: true, completion: nil)
+    }
+    
+    private func chooseFromGallery() {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            picker.allowsEditing = false
+            
+            present(picker, animated: true, completion: nil)
+        }
+    }
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[.originalImage] as? UIImage else {
+            applog("!!!!! Error")
+            return
+        }
+        profilePicture.image = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
