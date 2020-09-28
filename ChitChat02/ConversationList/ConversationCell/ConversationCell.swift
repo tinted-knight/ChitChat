3fe0c3b5b0d5d4d5ab7a9f3472bd03f75dcc9f10
Tim <the.tinted.knight@gmail.com>
2020-09-28 14:41:43 +0300

    Chat list
    - Conversation cell draft
    - Table delegates

---
+++ ChitChat02/ConversationList/ConversationCell/ConversationCell.swift
@@ -0,0 +1,42 @@
+//
+//  ConversationCell.swift
+//  ChitChat02
+//
+//  Created by Timun on 28.09.2020.
+//  Copyright © 2020 TimunInc. All rights reserved.
+//
+
+import UIKit
+
+protocol ConfigurableView {
+    associatedtype ConfigurationModel
+    
+    func configure(with model: ConfigurationModel)
+}
+
+class ConversationCell: UITableViewCell {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+extension ConversationCell: ConfigurableView {
+    func configure(with model: ConversationCellModel) {
+        name.text = model.name
+        message.text = model.message
+    }
+}
--- ChitChat02/ConversationList/ConversationListViewController.swift
+++ ChitChat02/ConversationList/ConversationListViewController.swift
@@ -11,6 +11,26 @@ import UIKit
 class ConversationListViewController: UIViewController {
 
     @IBOutlet weak var chatTableView: UITableView!
+
+    private let cellReuseId = "chat-list-cell"
+    
+    private let fakeChatList = [
+        ConversationCellModel(
+            name: "name1",
+            message: "message1",
+            date: Date(),
+            isOnline: true,
+            hasUnreadMessages: true
+        ),
+        ConversationCellModel(
+            name: "name2",
+            message: "message2",
+            date: Date(),
+            isOnline: false,
+            hasUnreadMessages: false
+        ),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,5 +39,32 @@ class ConversationListViewController: UIViewController {
 
     private func prepareUi() {
         title = "Tinkoff Chat"
+
+        chatTableView.register(UINib(nibName: "ConversationCell", bundle: nil), forCellReuseIdentifier: cellReuseId)
+        chatTableView.dataSource = self
+        chatTableView.delegate = self
     }
+}
+
+extension ConversationListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = chatTableView.dequeueReusableCell(withIdentifier: cellReuseId, for: indexPath)
+            as? ConversationCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: fakeChatList[indexPath.row])
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fakeChatList.count
+    }
+}
+
+extension ConversationListViewController: UITableViewDelegate {
+    
 }
