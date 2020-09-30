0f0dce679351c6029964a812a898ffe84d7b65ac
Tim <the.tinted.knight@gmail.com>
2020-09-30 13:43:31 +0300

    Fake data refactoring

--- ChitChat02/tools/fake_conversation.swift
+++ ChitChat02/tools/fake_conversation.swift
@@ -8,28 +8,20 @@
 
 import Foundation
 
-private func randomDirection() -> MessageDirection {
-    if Bool.random() {
-        return .income
-    } else {
-        return .outcome
-    }
-}
-
 let fakeMessages = [
-    MessageCellModel(text: "message text newest", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text 1", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text 2", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text 3", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
-    MessageCellModel(text: "message text oldest\nmessage text\nmessage text\n", date: fakeDate(), direction: randomDirection()),
+    MessageCellModel(text: "Да? Интересно.", date: fakeDate(), direction: .income),
+    MessageCellModel(text: "Потому что они искренне верят, что вы продали душу дьяволу", date: fakeDate(), direction: .outcome),
+    MessageCellModel(text: "Почему?", date: fakeDate(), direction: .income),
+    MessageCellModel(text: "Кстати, насчет ада - не помню, говорил я вам или нет. Знаете, почему нас так долго не трогают эти ткачи?", date: fakeDate(), direction: .outcome),
+    MessageCellModel(text: "Правильно, Петька. Давай лучше выпьем", date: fakeDate(), direction: .income),
+    MessageCellModel(text: "Хорошо, хорошо. Только не надо по бутылке стрелять", date: fakeDate(), direction: .outcome),
+    MessageCellModel(text: "Покажите", date: fakeDate(), direction: .outcome),
+    MessageCellModel(text: "Показать?", date: fakeDate(), direction: .income),
+    MessageCellModel(text: "Не понимаю, что вы имеете в виду.", date: fakeDate(), direction: .outcome),
+    MessageCellModel(text: "Петька, - сказал он, - смотри и запоминай. Если ты настоящий, то действительно смерть придет. Даже я тебе помочь не смогу. Я тебя еще раз спрашиваю. Вот стаканы, вот бутылка. Какая из этих форм настоящая?", date: fakeDate(), direction: .income),
+    MessageCellModel(text: "Обе. Обе настоящие.", date: fakeDate(), direction: .outcome),
+    MessageCellModel(text: "Меня? Никогда. Вот смотри. Петька. Самогон сам по себе не имеет формы. Вот стакан, вот блюдце. Какая из этих форм настоящая?", date: fakeDate(), direction: .income),
+    MessageCellModel(text: "Я согласен, - сказал я, садясь напротив, - все это замечательно, водоворот мыслей и так далее. Мир делается реальным и нереальным, я это все хорошо понимаю. Но сейчас сюда придут очень неприятные личности... Понимаете, я не хочу сказать, что они реальны, но нас они заставят ощутить свою реальность в полной мере", date: fakeDate(), direction: .outcome),
+    MessageCellModel(text: "Что поделать, Петька, - так уж устроен этот мир, что на все вопросы приходится отвечать посреди горящего дома.", date: fakeDate(), direction: .income),
+    MessageCellModel(text: "Слушайте, Чапаев, они, кажется подожгли усадьбу.", date: fakeDate(), direction: .outcome),
 ]
