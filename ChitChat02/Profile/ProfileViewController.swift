ce0ec49c08f310ca68703985de0bb98dd4204790
Tim <the.tinted.knight@gmail.com>
2020-09-30 11:12:31 +0300

    Profile screen
    - Navigation bar
    - Close NavBarItem

--- ChitChat02/Profile/ProfileViewController.swift
+++ ChitChat02/Profile/ProfileViewController.swift
@@ -19,17 +19,18 @@ class ProfileViewController : UIViewController {
     
     private let fakeUserName = "Timur Tharkahov"
     private let fakeUserDescription = """
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
-UX/UI designer, web-designer, Moscow, Russia
+Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.
+Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.
 """
     
     private let picker = UIImagePickerController()
@@ -82,6 +83,14 @@ UX/UI designer, web-designer, Moscow, Russia
     }
     @IBAction func onEditButtonTap(_ sender: Any) {
         showChooseDialog()
+    }
+    
+    @IBAction func onCloseTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onSaveTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     private func showAlert(_ message: String) {
