eed2ff0276ac1f7baa8aba74bb1fd3432fa5da4a
Tim <the.tinted.knight@gmail.com>
2020-10-12 22:57:04 +0300

    refactoring
    - GCDDataManager

--- ChitChat02/DataManager/GCDDataManager.swift
+++ ChitChat02/DataManager/GCDDataManager.swift
@@ -27,81 +27,35 @@ class GCDDataManager: DataManager {
     var user: UserModel = newUser
     var delegate: DataManagerDelegate?
 
+    private var nameResult: SaveResult = .success
+    private var descResult: SaveResult = .success
+    private var avatarResult: SaveResult = .success
+
     func save(_ model: UserModel, avatar: Data?) {
         applog("gcd save")
-        var nameResult: SaveResult = .success
-        var descResult: SaveResult = .success
-        var avatarResult: SaveResult = .success
 
         let group = DispatchGroup()
-
         if avatar != nil {
-            group.enter()
-            queue.asyncAfter(deadline: .now() + fakeDelay) { [weak self] in
-                guard let avatarUrl = self?.avatarUrl(), let avatarData = avatar else {
-                    avatarResult = .error
-                    group.leave()
-                    return
-                }
-                do {
-                    try avatarData.write(to: avatarUrl)
-//                    avatarResult = .error
-                    group.leave()
-                } catch {
-                    avatarResult = .error
-                    group.leave()
-                }
-            }
+            performAvatarTask(group, avatar: avatar)
         }
-
+        
         if model.name != user.name {
-            group.enter()
-            queue.asyncAfter(deadline: .now() + doubleDelay) { [weak self] in
-                guard let nameUrl = self?.nameUrl() else {
-                    nameResult = .error
-                    group.leave()
-                    return
-                }
-                do {
-                    try model.name.write(to: nameUrl, atomically: true, encoding: .utf8)
-//                    nameResult = .error
-                    group.leave()
-                } catch {
-                    nameResult = .error
-                    group.leave()
-                    return
-                }
-            }
+            perforNameTask(group, name: model.name)
         }
 
         if model.description != user.description {
-            group.enter()
-            queue.asyncAfter(deadline: .now() + fakeDelay) { [weak self] in
-                guard let descUrl = self?.descriptionUrl() else {
-                    descResult = .error
-                    group.leave()
-                    return
-                }
-                do {
-                    try model.description.write(to: descUrl, atomically: true, encoding: .utf8)
-//                    descResult = .error
-                    group.leave()
-                } catch {
-                    descResult = .error
-                    group.leave()
-                    return
-                }
-            }
+            perforDescriptionTask(group, description: model.description)
         }
 
         group.notify(queue: queue) { [weak self, user] in
-            guard nameResult == .success, descResult == .success, avatarResult == .success else {
+            guard self?.nameResult == .success, self?.descResult == .success,
+                self?.avatarResult == .success else {
                 // если одно из полей удалось сохранить,
                 // запишем в user, чтобы показать пользователю
                 self?.user = UserModel(
-                        name: nameResult == .success ? model.name : user.name,
-                        description: descResult == .success ? model.description : user.description,
-                        avatar: avatarResult == .success ? model.avatar : user.avatar
+                    name: self?.nameResult == .success ? model.name : user.name,
+                    description: self?.descResult == .success ? model.description : user.description,
+                    avatar: self?.avatarResult == .success ? model.avatar : user.avatar
                 )
                 self?.delegate?.onSaveError("Не все данные удалось сохранить")
                 return
@@ -131,6 +85,65 @@ class GCDDataManager: DataManager {
 //                self?.delegate?.onLoadError("desc error")
             } catch {
                 self?.delegate?.onLoadError(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func performAvatarTask(_ group: DispatchGroup, avatar: Data?) {
+        group.enter()
+        queue.asyncAfter(deadline: .now() + fakeDelay) { [weak self] in
+            guard let avatarUrl = self?.avatarUrl(), let avatarData = avatar else {
+                self?.avatarResult = .error
+                group.leave()
+                return
+            }
+            do {
+                try avatarData.write(to: avatarUrl)
+                //                    avatarResult = .error
+                group.leave()
+            } catch {
+                self?.avatarResult = .error
+                group.leave()
+            }
+        }
+    }
+
+    private func perforNameTask(_ group: DispatchGroup, name: String) {
+        group.enter()
+        queue.asyncAfter(deadline: .now() + doubleDelay) { [weak self] in
+            guard let nameUrl = self?.nameUrl() else {
+                self?.nameResult = .error
+                group.leave()
+                return
+            }
+            do {
+                try name.write(to: nameUrl, atomically: true, encoding: .utf8)
+                //                    nameResult = .error
+                group.leave()
+            } catch {
+                self?.nameResult = .error
+                group.leave()
+                return
+            }
+        }
+    }
+
+    private func perforDescriptionTask(_ group: DispatchGroup, description: String) {
+        group.enter()
+        queue.asyncAfter(deadline: .now() + fakeDelay) { [weak self] in
+            guard let descUrl = self?.descriptionUrl() else {
+                self?.descResult = .error
+                group.leave()
+                return
+            }
+            do {
+                try description.write(to: descUrl, atomically: true, encoding: .utf8)
+                //                    descResult = .error
+                group.leave()
+            } catch {
+                self?.descResult = .error
+                group.leave()
+                return
             }
         }
     }
