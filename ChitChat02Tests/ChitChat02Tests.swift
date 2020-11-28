f5bdc4bddaced9b9c2c51f52ed14e9dae534ff3d
Tim <the.tinted.knight@gmail.com>
2020-11-28 18:27:59 +0300

    Looks like Firebase test issue fixed

---
+++ ChitChat02Tests/ChitChat02Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ChitChat02Tests.swift
+//  ChitChat02Tests
+//
+//  Created by Timun on 28.11.2020.
+//  Copyright © 2020 TimunInc. All rights reserved.
+//
+
+@testable import ChitChat02
+import XCTest
+
+class ChitChat02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- Podfile
+++ Podfile
@@ -1,11 +1,13 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '13.7'
+use_frameworks!
+inhibit_all_warnings!
 
 target 'ChitChat02' do
-  # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
-
-  # Pods for ChitChat02
   pod 'SwiftLint'
   pod 'Firebase/Firestore'
+  
+  target 'ChitChat02Tests' do
+    inherit! :search_paths
+  end
 end
